0fb6b40b53e7be9a886f99a9dcecf4e8624b837a
Mateus Domingos <mateus.domingos@liferay.com>
2026-01-19 23:56:04 -0300

    feat: implement Store class with properties and methods for budget management

---
+++ src/core/domain/Store/Store.ts
@@ -0,0 +1,23 @@
+interface StoreProps {
+  readonly id: string;
+  readonly name: string;
+  readonly monthlyBudgetInCents: number;
+}
+
+export class Store {
+  constructor(public readonly props: StoreProps) {
+    this.validateMonthlyBudget(props.monthlyBudgetInCents);
+  }
+
+  validateMonthlyBudget(monthlyBudgetInCents: number): void {
+    if (monthlyBudgetInCents < 0) {
+      throw new Error('Monthly budget cannot be negative');
+    }
+  }
+
+  updateMonthlyBudgetInCents(budgetInCents: number): Store {
+    this.validateMonthlyBudget(budgetInCents);
+
+    return new Store({ ...this.props, monthlyBudgetInCents: budgetInCents });
+  }
+}
