57bcfdb09d7da94d2b467cdc5dd7b004fa200ea4
Mateus Domingos <mateus.domingos@liferay.com>
2026-02-08 23:36:09 -0300

    refactor: reorganize components path

--- src/components/ui/field.tsx
+++ src/components/ui/field.tsx
@@ -4,8 +4,8 @@ import { useMemo } from 'react';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
-import { Label } from '@/ui/components/label';
-import { Separator } from '@/ui/components/separator';
+import { Label } from '@/components/ui/label';
+import { Separator } from '@/components/ui/separator';
 
 function FieldSet({ className, ...props }: React.ComponentProps<'fieldset'>) {
   return (
